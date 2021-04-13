98df1a7eca0f035be963dd1c7381dea2d7525a89
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2021-04-13 07:57:46 -0400

    Implemented Swagger

---
+++ Repository/AssignmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Repository
+{
+    public class AssignmentRepository : RepositoryBase<Assignment>, IAssignmentRepository
+    {
+        public AssignmentRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Assignment> GetAllAssignments(bool trackChanges) =>
+            FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
+
+        public Assignment GetAssignment(Guid companyId, bool trackChanges) =>
+            FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+                .SingleOrDefault();
+
+        public void CreateAssignment(Assignment assignment) => Create(assignment);
+
+        public IEnumerable<Assignment> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(x => ids.Contains(x.Id), trackChanges)
+                .ToList();
+
+        public void DeleteAssignment(Assignment assignment)
+        {
+            Delete(assignment);
+        }
+    }
+}
---
+++ Repository/CourseRepository.cs
@@ -0,0 +1,37 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class CourseRepository : RepositoryBase<Course>, ICourseRepository
+    {
+        public CourseRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Course> GetAllCourses(bool trackChanges) =>
+            FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
+
+        public Course GetCourse(Guid companyId, bool trackChanges) =>
+            FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+                .SingleOrDefault();
+
+        public void CreateCourse(Course course) => Create(course);
+
+        public IEnumerable<Course> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(x => ids.Contains(x.Id), trackChanges)
+                .ToList();
+
+        public void DeleteCourse(Course course)
+        {
+            Delete(course);
+        }
+    }
+}
