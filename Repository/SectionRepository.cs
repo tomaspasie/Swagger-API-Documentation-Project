cf49e1a48cb1d4d411a0e3eee5505dff45cf0c3e
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2021-04-13 07:58:11 -0400

    Implemented Swagger

---
+++ Repository/SectionRepository.cs
@@ -0,0 +1,37 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class SectionRepository : RepositoryBase<Section>, ISectionRepository
+    {
+        public SectionRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Section> GetAllSections(bool trackChanges) =>
+            FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
+
+        public Section GetSection(Guid companyId, bool trackChanges) =>
+            FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+                .SingleOrDefault();
+
+        public void CreateSection(Section section) => Create(section);
+
+        public IEnumerable<Section> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(x => ids.Contains(x.Id), trackChanges)
+                .ToList();
+
+        public void DeleteSection(Section section)
+        {
+            Delete(section);
+        }
+    }
+}
